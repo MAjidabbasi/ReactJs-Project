2faaaa52c09e490669f72418fcdd904094cbd5fb
majid <malilohar@khi.iba.edu.pk>
2020-06-13 09:52:14 +0500

    Added a Controlled Form in Contact Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
@@ -10,6 +11,7 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
 import {LEADERS} from '../shared/leaders';
+
 
 
 class Main extends Component {
@@ -47,16 +49,25 @@ class Main extends Component {
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
             comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
-    };
+    }
+    const AboutUs = () => {
+      return(
+        <About leaders = {this.state.leaders} />
+      )
+    }
+   
     return (
       
       <div>    
         <Header />
         <Switch>
         <Route path='/home' component={HomePage} />
-        <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />        <Route path='/home' />
+        <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />        
+        <Route path='/home' />
         <Route path='/menu/:dishId' component={DishWithId} />
-        <Route exact path='/contactus' component={Contact} />} />
+        <Route exact path='/contactus' component={Contact} />
+        <Route exact path='/aboutus' component={AboutUs} />
+      
         
         <Redirect to='/home' />
         </Switch>
