445407067c514c128cb90d641c328b5560d47d50
majid <malilohar@khi.iba.edu.pk>
2020-06-21 15:02:40 +0500

    Fetch Posted Comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,7 +31,7 @@ class CommentForm extends Component {
     handleSubmit(values){
             // alert('States are : ' + JSON.stringify(values) );
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 
     }
 
@@ -114,7 +114,7 @@ class CommentForm extends Component {
 }
 
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
     if (comments != null) {
         return (
           <div >
@@ -138,7 +138,7 @@ function RenderComments({ comments, addComment, dishId }) {
 
               
 
-              <CommentForm dishId = {dishId} addComment = {addComment} />
+              <CommentForm dishId = {dishId} postComment = {postComment} />
           </div>
 
         );
@@ -208,7 +208,7 @@ const DishDetail = (props) => {
             </div>
             <div className="col-12 col-md-5 m-1">
                 <RenderComments comments={props.comments} 
-                    addComment = {props.addComment}
+                    postComment = {props.postComment}
                     dishId = {props.dish.id}
                 />
             </div>
